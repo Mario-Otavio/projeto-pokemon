836f305ec8f32d60ee30123061e08f48a5b298a7
mario <mariootaviosilva@gmail.com>
2025-06-22 21:03:15 -0400

    commit de ajuste

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
-import Comparar from "./comparar.jsx";
+import Comparar from "./Comparar.jsx";
 import PokemonDetail from "./PokemonDetail.jsx";
 
 const queryClient = new QueryClient();
